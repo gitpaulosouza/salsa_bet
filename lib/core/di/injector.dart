0e311a07fe427dc3396fd45828bace8444a33c19
paulo.gabriel.souza <paulo.gabriel.souza@accenture.com>
2025-10-02 02:41:45 -0300

    chore(di): register ThemeService and ThemeStore in dependency injector

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -2,8 +2,10 @@ import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:salsa_challenge/services/item/item_service.dart';
 import 'package:salsa_challenge/services/login/login_service.dart';
+import 'package:salsa_challenge/services/theme/theme_service.dart';
 import 'package:salsa_challenge/stores/favorites/favorite_store.dart';
 import 'package:salsa_challenge/stores/login/login_store.dart';
+import 'package:salsa_challenge/stores/theme/theme_store.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
@@ -15,10 +17,12 @@ Future<void> setupDependencies() async {
   final favoritesBox = await Hive.openBox('favorites_box');
 
   // Services
-  getIt.registerLazySingleton<LoginService>(() => LoginServiceImpl(prefs));
+  getIt.registerLazySingleton<LoginService>(() => LoginServiceImpl(getIt()));
   getIt.registerLazySingleton<ItemService>(() => MockItemService());
+  getIt.registerLazySingleton<ThemeService>(() => ThemeServiceImpl(getIt()));
 
   // Stores
-  getIt.registerFactory<LoginStore>(() => LoginStore(getIt<LoginService>()));
+  getIt.registerFactory<LoginStore>(() => LoginStore(getIt()));
   getIt.registerLazySingleton<FavoritesStore>(() => FavoritesStore(favoritesBox));
+  getIt.registerLazySingleton<ThemeStore>(() => ThemeStore(getIt()));
 }
