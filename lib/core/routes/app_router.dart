f48170ea3f6d05321818c72b60bd559c427d7ab6
paulo.gabriel.souza <paulo.gabriel.souza@accenture.com>
2025-10-01 23:12:22 -0300

    feat(router): configure GoRouter with / and /home routes

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:go_router/go_router.dart';
+import 'package:salsa_challenge/core/di/injector.dart';
+import 'package:salsa_challenge/stores/login/login_store.dart';
+import 'package:salsa_challenge/ui/home/home_page.dart';
+import 'package:salsa_challenge/ui/login/login_page.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => LoginPage(
+        loginStore: getIt<LoginStore>(),
+      ),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => const HomePage(),
+    ),
+  ],
+);
