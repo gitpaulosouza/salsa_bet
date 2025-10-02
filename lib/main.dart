14e4cc833b9f0a22c777e65d38e206e9065c7369
paulo.gabriel.souza <paulo.gabriel.souza@accenture.com>
2025-10-02 02:42:18 -0300

    feat(app): implements ThemeStore in main.dart for reactive theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:salsa_challenge/core/di/injector.dart';
 import 'package:salsa_challenge/core/routes/app_router.dart';
+import 'package:salsa_challenge/stores/theme/theme_store.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupDependencies();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final themeStore = getIt<ThemeStore>();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
+    return Observer(
+      builder: (_) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: router,
+        themeMode: themeStore.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+      ),
     );
   }
 }
