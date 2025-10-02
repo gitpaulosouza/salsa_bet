1efe920daa2535a212071c9bebfeed06c7522789
paulo.gabriel.souza <paulo.gabriel.souza@accenture.com>
2025-10-01 23:19:11 -0300

    refactor(di): inject SharedPreferences into LoginServiceImpl

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -1,12 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:salsa_challenge/services/login/login_service.dart';
 import 'package:salsa_challenge/stores/login/login_store.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setupDependencies() async {
+  final prefs = await SharedPreferences.getInstance();
 
-  getIt.registerLazySingleton<LoginService>(() => LoginServiceImpl());
+  getIt.registerLazySingleton<LoginService>(() => LoginServiceImpl(prefs));
 
   getIt.registerFactory<LoginStore>(() => LoginStore(getIt<LoginService>()));
 }
