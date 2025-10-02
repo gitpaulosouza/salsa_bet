40f17ceee41589de38b0e35ed74fe6f951556f1e
paulo.gabriel.souza <paulo.gabriel.souza@accenture.com>
2025-10-02 01:55:44 -0300

    chore(assets): add AppAssets constants for icons and images

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAssets {
+  static const favicon = 'assets/icons/favicon.png';
+
+  static const poker = 'assets/images/poker.jpg';
+  static const bingo = 'assets/images/bingo.jpg';
+  static const slots = 'assets/images/slots.jpg';
+  static const blackjack = 'assets/images/blackjack.jpg';
+  static const roleta = 'assets/images/roleta.jpg';
+}
